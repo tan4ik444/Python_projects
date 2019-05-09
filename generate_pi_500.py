9ed819b9bbe5bf81d66d66badc99973319386925
tan4ik444 <47626073+tan4ik444@users.noreply.github.com>
2019-05-09 17:58:42 +0300

    Update generate_pi_500.py

--- generate_pi_500.py
+++ generate_pi_500.py
@@ -1,103 +1,53 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 3,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Welcome to Pi Calculator. Please enter the number of digits upto which the value of Pi should be calculated or enter quit to exit\n",
-      "4\n",
-      "3.1417\n"
-     ]
-    }
-   ],
-   "source": [
-    "\"\"\"\n",
-    "Name: generate_pi_500.py\n",
-    "Purpose: Get the value of Pi to n number of decimal digits (up to 500 digits after the dot)\n",
-    "Author: tan_4ik_444\n",
-    "Algorithm: Chudnovsky Algorithm\n",
-    "License: MIT\n",
-    "Module Dependencies:\n",
-    "Decimal is required for the Decimal data type which is much better than Float for big calculation\n",
-    "\"\"\"\n",
-    "\n",
-    "import decimal\n",
-    "from decimal import Decimal, getcontext, ROUND_HALF_UP #use ROUND_HALF_UP for mathematical rounding rule\n",
-    " \n",
-    "def factorial(k): #function for factorial calculation\n",
-    "    a=Decimal(1)\n",
-    "    for i in range (int(k)):\n",
-    "        if i ==0:\n",
-    "            a*=Decimal(1)\n",
-    "        else:\n",
-    "            a*=Decimal(i)\n",
-    "    #print (type(a))\n",
-    "    return Decimal(a)\n",
-    "          \n",
-    "def amount_calculation(k): #function for calculation the amount from the Chudnovsky Algorithm\n",
-    "    i=k+1\n",
-    "    amount = Decimal(0)\n",
-    "    for i in range (k):\n",
-    "        top = (Decimal(-1))**Decimal(i)*factorial(Decimal(6)*Decimal(i))*(Decimal(13591409)+Decimal(54514013)*Decimal(i))\n",
-    "        bottom = factorial(Decimal(3)*Decimal(i))*((factorial(i))**Decimal(3))*(Decimal(640320)**(Decimal(3)*Decimal(i)+Decimal(1.5)))\n",
-    "        amount += top/bottom\n",
-    "    return Decimal(amount)\n",
-    " \n",
-    "def start():  #the function of running the main calculation\n",
-    "    while True:\n",
-    "        print (\"Welcome to Pi Calculator. Please enter the number of digits upto which the value of Pi should be calculated or enter quit to exit\")\n",
-    "        entry = input()\n",
-    "        if entry == \"quit\":\n",
-    "            print (\"Thank you! Bye!\")\n",
-    "        elif not entry.isdigit():\n",
-    "            print (\"You did not enter a number. Try again\")\n",
-    "        elif entry.isdigit():\n",
-    "            if int(entry)>500:  #to set up the limit for calculation - 500 digits after the dot\n",
-    "                getcontext().prec = 500+1  \n",
-    "                print ('limit is 500')\n",
-    "            else:\n",
-    "                getcontext().prec = int(entry)+1\n",
-    "            x=amount_calculation(int(entry))\n",
-    "            pi=1/(Decimal(12)*x)  #the last calculation from the Chudnovsky Algorithm \n",
-    "            print (pi)\n",
-    "            return\n",
-    "      \n",
-    "if __name__=='__main__':\n",
-    "    start()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.1"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+"""
+Name: generate_pi_500.py
+Purpose: Get the value of Pi to n number of decimal digits (up to 500 digits after the dot)
+Author: tan_4ik_444
+Algorithm: Chudnovsky Algorithm
+License: MIT
+Module Dependencies:
+Decimal is required for the Decimal data type which is much better than Float for big calculation
+"""
+
+import decimal
+from decimal import Decimal, getcontext, ROUND_HALF_UP #use ROUND_HALF_UP for mathematical rounding rule
+ 
+def factorial(k): #function for factorial calculation
+    a=Decimal(1)
+    for i in range (int(k)):
+        if i ==0:
+            a*=Decimal(1)
+        else:
+            a*=Decimal(i)
+    #print (type(a))
+    return Decimal(a)
+          
+def amount_calculation(k): #function for calculation the amount from the Chudnovsky Algorithm
+    i=k+1
+    amount = Decimal(0)
+    for i in range (k):
+        top = (Decimal(-1))**Decimal(i)*factorial(Decimal(6)*Decimal(i))*(Decimal(13591409)+Decimal(54514013)*Decimal(i))
+        bottom = factorial(Decimal(3)*Decimal(i))*((factorial(i))**Decimal(3))*(Decimal(640320)**(Decimal(3)*Decimal(i)+Decimal(1.5)))
+        amount += top/bottom
+    return Decimal(amount)
+ 
+def start():  #the function of running the main calculation
+    while True:
+        print ("Welcome to Pi Calculator. Please enter the number of digits upto which the value of Pi should be calculated or enter quit to exit")
+        entry = input()
+        if entry == "quit":
+            print ("Thank you! Bye!")
+        elif not entry.isdigit():
+            print ("You did not enter a number. Try again")
+        elif entry.isdigit():
+            if int(entry)>500:  #to set up the limit for calculation - 500 digits after the dot
+                getcontext().prec = 500+1  
+                print ('limit is 500')
+            else:
+                getcontext().prec = int(entry)+1
+            x=amount_calculation(int(entry))
+            pi=1/(Decimal(12)*x)  #the last calculation from the Chudnovsky Algorithm 
+            print (pi)
+            return
+      
+if __name__=='__main__':
+    start()
